b1ce527ce4ef1966d138db6ddc8c14df8e1e912e
Andrew Jaques <andrewjaques@gmail.com>
2018-11-27 15:29:03 +0000

    Learning more about the CSV module, Pandas and Numpy

--- TestCaseTreePruner.py
+++ TestCaseTreePruner.py
@@ -5,27 +5,26 @@ import glob
 
 # Find TestSequence files in the specified directory:
 list_of_found_sequences = []
-for filename in glob.glob('F:\\9.36 Config Cleanup\\TestSequences\\*.xml', recursive=True): # Set the root of the sequences directory here
+for filename in glob.glob('F:\\9.36 Config Cleanup\\TestSequences\\*.xml', recursive=True):  # Set the root of the sequences directory here
     list_of_found_sequences.append(filename)
 
 
 # Find TestCases files in the specified directory:
 list_of_found_test_cases = []
-for filename in glob.glob('F:\\9.36 Config Cleanup\\9.36.x\\**\\*TestCases*.xml', recursive=True): # Set the root of the config file 'Working' directory here
+for filename in glob.glob('F:\\9.36 Config Cleanup\\9.36.x\\**\\*TestCases*.xml', recursive=True):  # Set the root of the config file 'Working' directory here
     if "requiredFilesOnly" not in filename:
         list_of_found_test_cases.append(filename)
 
 
 # Build list of required configs:
-list_of_required_configs = [] # Empty to start with
+list_of_required_configs = []  # Empty to start with
 for sequence in list_of_found_sequences:
     tree = ET.parse(sequence)
     root = tree.getroot()
     for config in root.iter('config'):
         if "EncoderConfigFiles" in config.get("path"):
             list_of_required_configs.append(ntpath.basename(config.get("path")))
-    print("Added", len(list_of_required_configs), "required configs to list.")
-
+    print("Found", len(list_of_required_configs), "required configs so far.")
 
 # Create simplified TestCases files:
 removed_counter = 0
